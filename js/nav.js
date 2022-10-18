981b980cd9c1e61792814eaaf47fc05536f2d739
Annie  Lin <a.popcorn2@gmail.com>
2022-10-17 21:30:41 -0700

    assignment 2

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 document.write('<div id="top-bar">\
 			<div id="nameDiv">\
-			<a href="index.html" style= "text-decoration:none; font-family: "><h1 class="name">al</h1></a>\
+			<a href="index.html" style= "text-decoration:none; font-family: "><h1 class="name">annie lin</h1></a>\
 			</div>\
 			<div id="links">\
 			<p style="text-align:right;">\
